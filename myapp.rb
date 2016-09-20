6680333974989ae1eac52cac37599aeb26cce7db
Steve Domino <domino.steve@gmail.com>
2016-09-19 19:53:28 -0600

    updated comment

--- myapp.rb
+++ myapp.rb
@@ -1,8 +1,8 @@
 # myapp.rb
 require "sinatra"
 
-# nanobox configuration; ; most apps bind to localhost by default, however we need
-# to allow connections from your host into the container
+# nanobox configuration; most apps bind to localhost by default, however we need
+# to allow connections from the host into the container
 set :bind, "0.0.0.0"
 set :port, "8080"
 
